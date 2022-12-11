ae4e3d4322168c6d192713bdede9e461959fc000
MaxX <33282787+m1644@users.noreply.github.com>
2022-12-11 09:36:55 +0300

    Add Task_02

---
+++ Task_02.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+/**
+ * Task_02
+ */
+public class Task_02 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        //ArrayList list = new ArrayList();
+        list.add(2809);
+  
+        for (Object o : list) {
+            System.out.println(o);
+        }
+    }
+}
