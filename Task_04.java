7c16c7ab3cc509f404eac46cb397a71b8c3a6412
MaxX <33282787+m1644@users.noreply.github.com>
2022-12-11 09:37:15 +0300

    Add Task_04

---
+++ Task_04.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Task_04
+ */
+public class Task_04 {
+
+    public static void main(String[] args) {
+        int day = 29;
+        int month = 9;
+        int year = 1990;
+        Integer[] date = { day, month, year };
+        List<Integer> d = Arrays.asList(date);
+        System.out.println(d); // [29, 9, 1990]
+    }
+}
